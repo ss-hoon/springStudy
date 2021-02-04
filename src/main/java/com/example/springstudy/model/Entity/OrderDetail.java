238c0626a92d16b34171e56194df415be389be92
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-04 10:29:57 +0900

    Entity(DB 테이블명) 추가

---
+++ src/main/java/com/example/springstudy/model/Entity/OrderDetail.java
@@ -0,0 +1,36 @@
+package com.example.springstudy.model.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class OrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderAt;
+
+    private String status;
+    private LocalDateTime arrivalDate;
+    private Integer quantity;
+    private BigDecimal totalPrice;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
---
+++ src/main/java/com/example/springstudy/model/Entity/User.java
@@ -0,0 +1,34 @@
+package com.example.springstudy.model.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // == table
+// @Table(name="user") 테이블의 이름이 동일하면 생략 가능
+public class User {
+    // @Column(name="id") 컬럼의 이름이 동일하면 생략 가능
+    @Id // 식별자
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 전략 옵션
+    private Long id;
+    private String account;
+    private String password;
+    private String status;
+    private String email;
+    private String phoneNumber;
+    private LocalDateTime registeredAt;
+    private LocalDateTime unregisteredAt;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
