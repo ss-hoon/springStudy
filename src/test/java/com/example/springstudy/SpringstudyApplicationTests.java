c17f442bde97ad94b79389026c083eb182af3b35
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-04 10:49:44 +0900

    Init

--- src/test/java/com/example/springstudy/SpringstudyApplicationTests.java
+++ src/test/java/com/example/springstudy/SpringstudyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringstudyApplicationTests {
+public class SpringstudyApplicationTests {
 
     @Test
     void contextLoads() {
