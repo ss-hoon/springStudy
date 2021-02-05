0739be93a6efc90d574974cfc7161eeed8b8c7a9
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-06 00:20:19 +0900

    delete 메소드 타입 수정

--- src/main/java/com/example/springstudy/ifs/CrudInterface.java
+++ src/main/java/com/example/springstudy/ifs/CrudInterface.java
@@ -6,5 +6,5 @@ public interface CrudInterface<Req, Res> {
     Header<Res> create(Header<Req> request);
     Header<Res> read(Long id);
     Header<Res> update(Header<Req> request);
-    Header<Res> delete(Long id);
+    Header delete(Long id);
 }
