8b1d34a51407f533b4236e1df7c3a822c567913e
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-07 01:37:12 +0900

    리팩토링

--- src/main/java/com/example/springstudy/service/OrderDetailApiLogicService.java
+++ src/main/java/com/example/springstudy/service/OrderDetailApiLogicService.java
@@ -45,7 +45,7 @@ public class OrderDetailApiLogicService implements CrudInterface<OrderDetailApiR
     @Override
     public Header<OrderDetailApiResponse> read(Long id) {
         return orderDetailRepository.findById(id)
-                .map(orderDetail -> this.response(orderDetail))
+                .map(this::response)
                 .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
