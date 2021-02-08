3f0a897d72072eb2ccd038e7bdd1b1cc9fe7848f
ss-hoon <46196555+ss-hoon@users.noreply.github.com>
2021-02-08 23:19:53 +0900

    ItemApi enum 적용

--- src/main/java/com/example/springstudy/model/entity/Item.java
+++ src/main/java/com/example/springstudy/model/entity/Item.java
@@ -1,5 +1,6 @@
 package com.example.springstudy.model.entity;
 
+import com.example.springstudy.model.enumClass.ItemStatus;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -27,7 +28,8 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // mysql
     private Long id;
 
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private ItemStatus status; // 등록, 해지, 검수중
 
     private String name;
 
